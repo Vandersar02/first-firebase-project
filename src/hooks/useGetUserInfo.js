ab6eba724c51cee3ed9d763ef7944ffbbae9f406
St Cyr Vandersar <leejvan02@gmail.com>
2024-06-13 14:03:01 -0400

    useGetUserInfo upDate

--- src/hooks/useGetUserInfo.js
+++ src/hooks/useGetUserInfo.js
@@ -1,6 +1,6 @@
 export const useGetUserInfo = () => {
   const { name, profilePhoto, userId, isAuth } = JSON.parse(
-    localStorage.getItem("auth")
+    localStorage.getItem("auth") || {}
   );
   return { name, profilePhoto, userId, isAuth };
 };
